2c9f24a615e36b10044046d69edb0ce77c8459db
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2021-08-16 21:27:15 +0800

    We won't need sign up anymore since google provider handles that, cleaned app module. added auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,28 +8,13 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import firebase from 'firebase';
-import * as firebaseui from 'firebaseui';
 
 import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-
-const firebaseUiAuthConfig: firebaseui.auth.Config = {
-  signInFlow: 'popup',
-  signInOptions: [
-    {
-      requireDisplayName: false,
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
-    }
-  ],
-  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
-};
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent,
-    SignUpComponent
+    SignInComponent
   ],
   imports: [
     BrowserModule,
