7bc3b2ba6bd16dd8f8cedbdfdd975d8e85f23386
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2021-08-17 00:16:47 +0800

    created dashboard component and also the logic for user sign in, if user is not signed in, then the dashboard should not be accessible

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from '../app/components/sign-in/sign-in.component';
 
 import { AuthGuard } from '../app/shared/guard/auth.guard';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,29 +1,40 @@
-import { Inject, Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { User } from './user';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  userData: any;
+  userData: User = {
+    uid: '',
+    email: '',
+    displayName: '',
+    photoURL: '',
+    emailVerified: false
+  };
 
   constructor(
-    public fireStore: AngularFirestore,
-    public fireAuth: AngularFireAuth,
-    public router: Router,
-    public ngZone: NgZone
+    private fireStore: AngularFirestore,
+    private fireAuth: AngularFireAuth,
+    private router: Router
   ) {
-    this.fireAuth.authState.subscribe(user => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.userData = user;
+        this.userData.uid = user.uid;
+        this.userData.email = user.email || '';
+        this.userData.displayName = user.displayName || '';
+        this.userData.emailVerified = user.emailVerified;
+        this.userData.photoURL = user.photoURL || '';
+
         localStorage.setItem('user', JSON.stringify(this.userData));
-        //JSON.parse(localStorage.getItem('user'));
+        JSON.parse(localStorage.getItem('user') || '{}');
       } else {
         localStorage.setItem('user', '');
-        //JSON.parse(localStorage.getItem('user'));
+        JSON.parse(localStorage.getItem('user') || '{}');
       }
     })
   }
@@ -31,7 +42,19 @@ export class AuthService {
   // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user') || '{}');
-    return (user !== null && user.emailVerified !== false) ? true : false;
+    return (Object.keys(user).length > 0) ? true : false;
+  }
+
+  // Sign out
+  SignOut() {
+    return this.fireAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['sign-in']);
+    })
+  }
+
+  OnSignInSuccessful(user: User): void {
+    this.SetUserData(user);
   }
 
   /* Setting up user data when sign in with username/password,
