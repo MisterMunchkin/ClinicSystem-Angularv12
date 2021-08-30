2a9eacb352d67288dc0580e22a49187b277e1760
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2021-08-30 18:32:56 +0800

    CLIN-16 :  error message set up and custom validators

--- src/app/components/patients/patient-form-dialog/patient-form-dialog.component.ts
+++ src/app/components/patients/patient-form-dialog/patient-form-dialog.component.ts
@@ -4,13 +4,14 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Patient, PatientHistory } from 'src/app/shared/models/patient';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { validatebirthDay, validatebirthMonth, validatebirthYear } from 'src/app/shared/validators/birthday-validator';
 
 @Component({
   selector: 'app-patient-form-dialog',
   templateUrl: './patient-form-dialog.component.html',
   styleUrls: ['./patient-form-dialog.component.css']
 })
-export class PatientFormDialogComponent {
+export class PatientFormDialogComponent implements OnInit {
   patientForm: FormGroup;
   civilStatusNames: string[] = CivilStatusDB.civilStatusNames;
   genderNames: string[] = GenderDB.genderNames;
@@ -22,22 +23,23 @@ export class PatientFormDialogComponent {
   public dialogRef: MatDialogRef<PatientFormDialogComponent>,
   @Inject(MAT_DIALOG_DATA) public data: Patient,
   private formBuilder: FormBuilder) {
-    console.log(data);
     this.patientData = JSON.parse(JSON.stringify(data));
+  }
 
-    this.patientForm = this.formBuilder.group({
-      address: data.address,
-      birthMonth: data.birthDate.month,
-      birthYear: data.birthDate.year,
-      birthDay: data.birthDate.day,
-      civilStatus: data.civilStatus,
-      firstName: data.firstName,
-      gender: data.gender,
-      lastName: data.lastName,
-      middleName: data.middleName
-    });
+  ngOnInit(){
+    this.patientForm = new FormGroup({
+      address: new FormControl(this.patientData.address),
+      birthMonth: new FormControl(this.patientData.birthDate.month, [Validators.required]),
+      birthYear: new FormControl(this.patientData.birthDate.year, [Validators.required]),
+      birthDay: new FormControl(this.patientData.birthDate.day, Validators.required),
+      civilStatus: new FormControl(this.patientData.civilStatus, Validators.required),
+      firstName: new FormControl(this.patientData.firstName, Validators.required),
+      gender: new FormControl(this.patientData.gender, Validators.required),
+      lastName: new FormControl(this.patientData.lastName, Validators.required),
+      middleName: new FormControl(this.patientData.middleName)
+    }, {validators: [validatebirthDay, validatebirthMonth, validatebirthYear]} );
 
-    this.patientHistoryCollection = data.patientHistory ?? [];
+    this.patientHistoryCollection = this.patientData.patientHistory ?? [];
   }
 
   onSubmit() {
@@ -54,5 +56,6 @@ export class PatientFormDialogComponent {
     this.patientData.middleName = formGroupValues.middleName;
 
     console.log(this.patientData);
+    console.log(this.patientForm);
   }
 }
