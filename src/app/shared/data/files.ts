adca13141befd6f58ed9f9c400a7929da99793c8
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2021-09-20 13:30:06 +0800

    CLIN-36 : added file validations on upload-task conponent.
    maximum file size - 5.2 mb
    blocked file types - executables and others
    duplicate names - document.rtf and document.mov can be in the same directory, but not document.rtf and document.rtf

--- src/app/shared/data/files.ts
+++ src/app/shared/data/files.ts
@@ -4,6 +4,9 @@ export class FilesHelper {
   //max  file size is 5.24mb
   public static maximumFileSize = 5242880;
 
+  //regex of common file types that should be blocked to prevent viruses
+  public static blockedFileTypesRegex: RegExp = /(\.|\/)(bat|exe|cmd|sh|php([0-9])?|pl|cgi|386|dll|com|torrent|js|app|jar|pif|vb|vbscript|wsf|asp|cer|csr|jsp|drv|sys|ade|adp|bas|chm|cpl|crt|csh|fxp|hlp|hta|inf|ins|isp|jse|htaccess|htpasswd|ksh|lnk|mdb|mde|mdt|mdw|msc|msi|msp|mst|ops|pcd|prg|reg|scr|sct|shb|shs|url|vbe|vbs|wsc|wsf|wsh)$/i;
+
   //File path is patient document id/ date of visit/ file.docx
   // public static getPatientHistoryFilePath(patient: Patient, patientHistory: PatientHistory){
   //   return patient.documentId + '/' + this.changeDateStringFormat(patientHistory.dateOfVisit) + '/';
