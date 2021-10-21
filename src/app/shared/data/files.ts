0e6554f16e8b05c25de59d84f950c8ab47ecf74a
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2021-10-21 15:36:42 +0800

    CLIN-46
    Changed file path for patient file path. It now uses firebase buckets to organize based on clinics -> Modules (Patients) -> patient id -> documents

--- src/app/shared/data/files.ts
+++ src/app/shared/data/files.ts
@@ -1,3 +1,4 @@
+import { Clinic } from "../models/clinic";
 import { Patient, PatientHistory } from "../models/patient";
 
 export class FilesHelper {
@@ -10,12 +11,11 @@ export class FilesHelper {
   //regex of common file types that should be blocked to prevent viruses
   public static blockedFileTypesRegex: RegExp = /(\.|\/)(bat|exe|cmd|sh|php([0-9])?|pl|cgi|386|dll|com|torrent|js|app|jar|pif|vb|vbscript|wsf|asp|cer|csr|jsp|drv|sys|ade|adp|bas|chm|cpl|crt|csh|fxp|hlp|hta|inf|ins|isp|jse|htaccess|htpasswd|ksh|lnk|mdb|mde|mdt|mdw|msc|msi|msp|mst|ops|pcd|prg|reg|scr|sct|shb|shs|url|vbe|vbs|wsc|wsf|wsh)$/i;
 
-  //File path is patient document id/ date of visit/ file.docx
-  // public static getPatientHistoryFilePath(patient: Patient, patientHistory: PatientHistory){
-  //   return patient.documentId + '/' + this.changeDateStringFormat(patientHistory.dateOfVisit) + '/';
-  // }
+  //Stores the files in buckets of clinic id
+  //organizes them by Patients/patient id
   public static getPatientHistoryFilePath(patient: Patient){
-    return patient.documentId + '/';
+    const clinic: Clinic = JSON.parse(localStorage.getItem('clinic') ?? '');
+    return clinic.id + '/Patients/' + patient.documentId + '/';
   }
 
   static changeDateStringFormat(date: string){
