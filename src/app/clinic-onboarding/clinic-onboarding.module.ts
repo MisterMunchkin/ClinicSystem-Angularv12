8b43863f176c4dbb73061c0068521c807e9592a8
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-01-21 13:39:02 +0800

    front end for clinic onboarding form CLIN-57

--- src/app/clinic-onboarding/clinic-onboarding.module.ts
+++ src/app/clinic-onboarding/clinic-onboarding.module.ts
@@ -1,9 +1,20 @@
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClinicOnboardingComponent } from './clinic-onboarding/clinic-onboarding.component';
 import { ClinicOnboardingRoutingModule } from './clinic-onboarding-routing.module';
 
-
+const uiModules = [
+  MatSlideToggleModule,
+  MatFormFieldModule,
+  MatInputModule,
+  ReactiveFormsModule,
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [
@@ -11,7 +22,11 @@ import { ClinicOnboardingRoutingModule } from './clinic-onboarding-routing.modul
   ],
   imports: [
     CommonModule,
-    ClinicOnboardingRoutingModule
+    ClinicOnboardingRoutingModule,
+    uiModules
+  ],
+  exports: [
+    uiModules
   ]
 })
 export class ClinicOnboardingModule { }
--- src/app/clinic-onboarding/clinic-onboarding/clinic-onboarding.component.ts
+++ src/app/clinic-onboarding/clinic-onboarding/clinic-onboarding.component.ts
@@ -1,3 +1,5 @@
+import { UserClinic } from './../../shared/models/user';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clinic-onboarding.component.css']
 })
 export class ClinicOnboardingComponent implements OnInit {
+  clinicForm: FormGroup;
+  clinicData: UserClinic = {
+    id: '',
+    name: '',
+    address: ''
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clinicForm = new FormGroup({
+      name: new FormControl(this.clinicData.name, [Validators.required]),
+      address: new FormControl(this.clinicData.address)
+    })
   }
 
+  onSubmit() {
+    if (this.clinicForm.valid) {
+      //should insert to clinic collection and also add the clinic to the user clinic array
+    }
+  }
 }
